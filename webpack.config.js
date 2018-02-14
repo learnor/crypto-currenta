f16cdf207d90b4d00f98d9c4c826d54319cf9e18
learnor <learnor@gmail.com>
2018-02-14 10:18:41 -0800

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+// const webpack = require('webpack');
+
+module.exports = {
+  entry: "./lib/main.js",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/,/\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env'],
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extentions: ['.js','*']
+  },
+};
